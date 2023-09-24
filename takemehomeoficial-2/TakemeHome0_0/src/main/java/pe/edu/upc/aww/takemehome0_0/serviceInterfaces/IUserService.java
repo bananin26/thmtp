3a4698a3aa67de9cb412d8b6854c2bdee7d4bada
bananin26 <144152663+bananin26@users.noreply.github.com>
2023-09-23 23:46:56 -0500

    Add files via upload

---
+++ takemehomeoficial-2/TakemeHome0_0/src/main/java/pe/edu/upc/aww/takemehome0_0/serviceInterfaces/IUserService.java
@@ -0,0 +1,15 @@
+package pe.edu.upc.aww.takemehome0_0.serviceinterfaces;
+
+import pe.edu.upc.aww.takemehome0_0.entities.User;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface IUserService {
+
+    public void insert(User user);
+    public List<User> list();
+    public void delete(int idUser);
+    public User listId(int idUser);
+    List<User>findByBirthday(LocalDate birthday);
+}
